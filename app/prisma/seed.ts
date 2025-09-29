d83b6b85fecd5e6c69d8d1096b3b3012676d6bcc
Sam <samlangenfeld@gmail.com>
2025-09-29 14:14:16 -0400

    add prisma migrations for user-org-membership models

---
+++ app/prisma/seed.ts
@@ -0,0 +1,79 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@demo.com",
+      name: "Alice",
+      password: "demo",
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@demo.com",
+      name: "Bob",
+      password: "demo",
+    },
+  });
+  const org = await prisma.organization.create({
+    data: {
+      name: "Demo Org",
+    },
+  });
+  const adminRole = await prisma.role.upsert({
+    where: { name: "admin" },
+    update: {},
+    create: {
+      name: "admin",
+    },
+  });
+  const userRole = await prisma.role.upsert({
+    where: { name: "user" },
+    update: {},
+    create: {
+      name: "user",
+    },
+  });
+  await prisma.membership.upsert({
+    where: {
+      userId_orgId: {
+        userId: bob.id,
+        orgId: org.id,
+      },
+    },
+    update: {},
+    create: {
+      userId: bob.id,
+      orgId: org.id,
+      roleId: adminRole.id,
+    },
+  });
+  await prisma.membership.upsert({
+    where: {
+      userId_orgId: {
+        userId: alice.id,
+        orgId: org.id,
+      },
+    },
+    update: {},
+    create: {
+      userId: alice.id,
+      orgId: org.id,
+      roleId: userRole.id,
+    },
+  });
+  console.log({ alice, bob });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
